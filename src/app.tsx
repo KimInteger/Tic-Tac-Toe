d9e2921fae8f5b524fd60b68d212e408fb903d01
김정수 <kjs20151240@gmail.com>
2024-08-29 11:50:30 +0900

    fire | app.tsx
    TicTacToeField.tsx 하나만 호출

--- src/app.tsx
+++ src/app.tsx
@@ -4,19 +4,7 @@ import TicTacToeField from './app/gameBoard/playField';
 const App: React.FC = () => {
   return (
     <>
-      <div>
-        <h1>A</h1>
-        <h2>
-          Score : <span></span>
-        </h2>
-      </div>
       <TicTacToeField />
-      <div>
-        <h1>B</h1>
-        <h2>
-          Score : <span></span>
-        </h2>
-      </div>
     </>
   );
 };
